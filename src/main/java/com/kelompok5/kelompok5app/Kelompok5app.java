32fe23e1dbdaa12a3f95d67a6b6f09621b27dbb0
FauzanIlyasAlmeyda <ggid.uzen@gmail.com>
2025-05-13 20:28:55 +0700

    class awal

---
+++ src/main/java/com/kelompok5/kelompok5app/Kelompok5app.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kelompok5.kelompok5app;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Kelompok5app {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
